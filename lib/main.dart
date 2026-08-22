7969980563cdd83f1504a95d6f27a8168c80ab54
mhmdalhakm864 <mhmdalhakm864@gmail.com>
2026-08-23 02:15:06 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
-
+import 'screens/ai_chat_screen.dart';
 void main() {
   runApp(const MetaMootProALApp());
 }
